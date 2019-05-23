90f23e42a3f2ebd76fbb8e0c3d15c07fbe7b8093
Nicki Nixon <nickitza@gmail.com>
2019-05-23 15:16:54 -0600

    board index

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,5 +1,9 @@
 class BoardsController < ApplicationController
+
+  before_action :set_board, only:[:show, :edit, :update, :destroy]
+
   def index
+    @boards = Board.all_boards(current_user.id)
   end
 
   def show
@@ -8,6 +12,20 @@ class BoardsController < ApplicationController
   def new
   end
 
+  def create
+  end
+
   def edit
   end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private def set_board
+    @board = Board.single_board(current_user.id, params[:id])
+  end
+
 end
--- app/models/board.rb
+++ app/models/board.rb
@@ -1,3 +1,21 @@
 class Board < ApplicationRecord
   belongs_to :user
+
+  # === CLASS METHODS =====
+
+  def self.all_boards(user_id)
+    Board.find_by_sql("
+      SELECT *
+      FROM boards
+      WHERE boards.user_id = #{user_id}
+      ")
+  end
+
+  def self.single_board(user_id, board_id)
+    Board.find_by_sql(["
+      SELECT
+      FROM boards
+      "])
+  end
+
 end
