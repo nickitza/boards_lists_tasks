adfef0611d7964f9d3c69f2d46968b7d27360e49
Nicki Nixon <nickitza@gmail.com>
2019-05-26 12:09:57 -0600

    user name, boards index views

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :boards, dependent: :destroy
+
+  def full_name
+    "#{self.first_name} #{self.last_name}"
+  end
 end
